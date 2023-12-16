82cf18aa63ea6d45da473e2520999c45ac41e38d
Kevin Stillhammer <kevin.stillhammer@gmail.com>
2023-12-16 12:40:27 +0100

    style: use fixture for respx

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,5 +1,4 @@
 """Test fressnapf_tracker setup process."""
-from httpx import Response
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
 from custom_components.fressnapf_tracker import (
@@ -12,20 +11,8 @@ from custom_components.fressnapf_tracker.const import DOMAIN
 from .const import MOCK_CONFIG
 
 
-async def test_setup_unload_and_reload_entry(hass, respx_mock):
+async def test_setup_unload_and_reload_entry(hass, get_response):
     """Test entry setup and unload."""
-    respx_mock.get(
-        "https://itsmybike.cloud/api/pet_tracker/v2/devices/test_serialnumber?devicetoken=test_device_token"
-    ).mock(
-        return_value=Response(
-            200,
-            json={
-                "name": "Test",
-                "position": {"lat": 1, "lng": 1, "accuracy": 0},
-                "tracker_settings": {"generation": "2.1"},
-            },
-        )
-    )
     config_entry = MockConfigEntry(domain=DOMAIN, data=MOCK_CONFIG, entry_id="test")
 
     assert await async_setup_entry(hass, config_entry)
